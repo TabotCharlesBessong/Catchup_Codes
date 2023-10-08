e1fb3eb5fecec11baae298cf632726ab96571da4
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2023-10-08 22:31:52 +0100

    Delete podifyPlayer/dist directory

--- podifyPlayer/dist/src/api/client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-var client = axios_1.default.create({
-    baseURL: "http://192.168.1.153:5000",
-});
-exports.default = client;
--- podifyPlayer/dist/src/store/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var toolkit_1 = require("@reduxjs/toolkit");
-var auth_1 = require("./auth");
-var store = (0, toolkit_1.configureStore)({
-    reducer: auth_1.default
-});
-exports.default = store;
