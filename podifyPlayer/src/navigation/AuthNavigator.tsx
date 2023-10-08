03a06b9efc70eff2668976ed2cfc720b95c6c1f3
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-08 10:07:04 +0100

    setting up basic auth slice

--- podifyPlayer/src/navigation/AuthNavigator.tsx
+++ podifyPlayer/src/navigation/AuthNavigator.tsx
@@ -3,12 +3,17 @@ import ForgotPassword from "@views/auth/ForgotPassword"
 import SignIn from "@views/auth/SignIn"
 import SignUp from "@views/auth/Signup"
 import Verification from "@views/auth/Verification"
+import { useSelector } from "react-redux"
 import { AuthStackParamList } from "src/@types/navigation"
+import { getAuthState } from "src/store/auth"
 const React = require("react")
 
 const Stack = createNativeStackNavigator<AuthStackParamList>()
 
 const AuthNavigator = () => {
+  const authState = useSelector(getAuthState)
+  console.log(authState)
+  
   return (
     <Stack.Navigator screenOptions={{
       headerShown:false
