efcc9ad90149d826245e2d277f91abb7d05b3e8b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-18 17:30:53 +0100

    testing the notification component

--- podifyPlayer/src/views/Home.tsx
+++ podifyPlayer/src/views/Home.tsx
@@ -1,13 +1,19 @@
 import React = require('react');
 import {FC} from 'react';
-import {View, StyleSheet,Text} from 'react-native';
+import {View, StyleSheet,Text, Button} from 'react-native';
+import { useDispatch } from 'react-redux';
+import { upldateNotification } from 'src/store/notification';
 
 interface Props {}
 
 const Home: FC<Props> = (props) => {
+  const dispatch = useDispatch()
   return (
     <View style={styles.container}>
       <Text>Hello Home</Text>
+      <Button title='Test' onPress={() => {
+        dispatch(upldateNotification({message:"Just for testing",type:'error'}))
+      }} />
     </View>
   )
 };
