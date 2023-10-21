a09c89e27dbca887df159a15df18c656cdf10e01
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-21 15:04:18 +0100

    refactoring latest uploads

--- podifyPlayer/src/views/Home.tsx
+++ podifyPlayer/src/views/Home.tsx
@@ -1,11 +1,13 @@
 import PulseAnimationContainer from "@ui/PulseAnimationContainer";
+import colors from "@utils/colors";
 import React = require("react");
 import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { useQuery } from "react-query";
 import { useDispatch } from "react-redux";
 import catchAsyncError from "src/api/catchError";
 import client from "src/api/client";
+import LatestUploads from "src/component/LatestUploads";
 import { useFetchLatestAudios } from "src/hooks/query";
 import { upldateNotification } from "src/store/notification";
 
@@ -14,7 +16,7 @@ interface Props {}
 const Home: FC<Props> = (props) => {
   const { data, isLoading } = useFetchLatestAudios();
 
-  // if (isLoading) {
+  if (isLoading) {
     return (
       <PulseAnimationContainer>
         <Text
@@ -23,36 +25,24 @@ const Home: FC<Props> = (props) => {
             fontSize: 32,
             textAlign: "center",
             alignSelf: "center",
-            marginTop:20
+            marginTop: 20,
           }}
         >
           Lauding....
         </Text>
       </PulseAnimationContainer>
     );
-  // }
+  }
   return (
     <View style={styles.container}>
-      {/* <Text>Hello Home</Text> */}
-      {data?.map((item) => (
-        <Text
-          style={{
-            color: "white",
-            fontSize: 16,
-            paddingVertical:10
-          }}
-          key={item.id}
-        >
-          {item.title}
-        </Text>
-      ))}
+      <LatestUploads />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
+    padding: 20,
     marginTop: 20,
   },
 });
