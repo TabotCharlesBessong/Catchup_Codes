e4aec7ef70dba29db8895ed318cccc34be4506c6
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-21 15:10:44 +0100

    updating loading

--- podifyPlayer/src/views/Home.tsx
+++ podifyPlayer/src/views/Home.tsx
@@ -1,38 +1,12 @@
-import PulseAnimationContainer from "@ui/PulseAnimationContainer";
-import colors from "@utils/colors";
-import React = require("react");
 import { FC } from "react";
-import { Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
-import { useQuery } from "react-query";
-import { useDispatch } from "react-redux";
-import catchAsyncError from "src/api/catchError";
-import client from "src/api/client";
+import { StyleSheet, View } from "react-native";
 import LatestUploads from "src/component/LatestUploads";
-import { useFetchLatestAudios } from "src/hooks/query";
-import { upldateNotification } from "src/store/notification";
+import React = require("react");
 
 interface Props {}
 
 const Home: FC<Props> = (props) => {
-  const { data, isLoading } = useFetchLatestAudios();
 
-  if (isLoading) {
-    return (
-      <PulseAnimationContainer>
-        <Text
-          style={{
-            color: "white",
-            fontSize: 32,
-            textAlign: "center",
-            alignSelf: "center",
-            marginTop: 20,
-          }}
-        >
-          Lauding....
-        </Text>
-      </PulseAnimationContainer>
-    );
-  }
   return (
     <View style={styles.container}>
       <LatestUploads />
