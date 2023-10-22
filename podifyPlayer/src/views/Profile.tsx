3758c9d33fc5f9fa4a0a10b1f46cbf105ec94317
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-22 15:41:51 +0100

    top tab navigation

--- podifyPlayer/src/views/Profile.tsx
+++ podifyPlayer/src/views/Profile.tsx
@@ -1,19 +1,35 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { FC } from 'react';
+import { StyleSheet, View } from 'react-native';
+import FavoriteTab from 'src/component/profile/FavoriteTab';
+import PlaylistTab from 'src/component/profile/PlaylistTab';
+import UploadsTab from 'src/component/profile/UploadsTab';
 import React = require('react');
-import {FC} from 'react';
-import {View, StyleSheet,Text} from 'react-native';
+import HistoryTab from 'src/component/profile/HistoryTab';
 
 interface Props {}
+
+const Tab = createMaterialTopTabNavigator()
 
 const Profile: FC<Props> = (props) => {
   return (
     <View style={styles.container}>
-      <Text>Hello Profile</Text>
+      <Tab.Navigator>
+        <Tab.Screen name="Uploads" component={UploadsTab} />
+        <Tab.Screen name="Playlist" component={PlaylistTab} />
+        <Tab.Screen name="Favourites" component={FavoriteTab} />
+        <Tab.Screen name="History" component={HistoryTab} />
+      </Tab.Navigator>
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex:1,
+    padding:20,
+    marginTop:20
+  },
 });
 
 export default Profile
